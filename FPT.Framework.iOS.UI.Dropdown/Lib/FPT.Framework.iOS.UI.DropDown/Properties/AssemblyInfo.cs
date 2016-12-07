89524b19155626af6af84fb9fd51841984cfe4f6
phamquan <mr.pquan@gmail.com>
2016-12-07 10:42:32 +0700

    set dell description

--- FPT.Framework.iOS.UI.Dropdown/Lib/FPT.Framework.iOS.UI.DropDown/Properties/AssemblyInfo.cs
+++ FPT.Framework.iOS.UI.Dropdown/Lib/FPT.Framework.iOS.UI.DropDown/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("FPT.Framework.iOS.UI.DropDown")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Part of FPT.Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("FPT Information System")]
+[assembly: AssemblyProduct("FPT.Framework.iOS.UI.DropDown")]
 [assembly: AssemblyCopyright("phamquan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
